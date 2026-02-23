12b0ea65ae2e3c0018a4ee91f72c4f3e13473007
Gael Castro <gaelcastrolara2006@gmail.com>
2026-02-23 14:55:22 -0600

    initialization of the server

--- back-end/server.js
+++ back-end/server.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path'); // Importante para manejar rutas de carpetas
 const app = express();
-const session = require('express-session'); // Para manejar sesiones
-const connectDB = require('./db.js'); // Importar la conexión a la base de datos
+// const connectDB = require('./db.js'); // Importar la conexión a la base de datos
 const PORT = 4000;
 
 
 app.use(express.json()); // Middleware para parsear JSON
 
-const publicPath = path.join(__dirname, 'front-end');
+const publicPath = path.join(__dirname, '..', 'front-end');
 app.use(express.static(publicPath));
 
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor ejecutando en http://localhost:${PORT}`);
