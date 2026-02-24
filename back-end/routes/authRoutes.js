8da9e6945ddb6564796e4bc6b5641a47eafeda6f
ARMET6 <adrian.razo.mandujano.11@gmail.com>
2026-02-23 18:39:22 -0600

    Delete authRoutes.js

--- back-end/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const { validateRegister } = require('../middlewares/validator');
-
-router.post('/register', validateRegister, authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
