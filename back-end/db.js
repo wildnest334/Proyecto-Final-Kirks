d00e747af64728fb8ab81d0536236b4528e5d975
mauricioglpz <Al07023486@tecmilenio.mx>
2026-02-23 18:46:19 -0600

    Add files via upload

---
+++ back-end/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Conexión exitosa a MongoDB Atlas');
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error.message);
+    process.exit(1); 
+  }
+};
+
+module.exports = connectDB;
